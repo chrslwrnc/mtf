d30816d24d4d14757ade5bdc72113da9f1ec224d
Chris Lawrence <chris@nanimokamo.com>
2017-03-31 16:21:05 +0100

    add postinstall step to pkg

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const compression = require('compression');
 const path = require('path');
 const app = express();
+console.log('PATH2:', path.resolve(__dirname, '../dist/index.html'));
 const index = (req, res) => res.sendFile(path.resolve(__dirname, '../dist/index.html'));
 
 app.use(compression());
